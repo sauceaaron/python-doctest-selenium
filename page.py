c405ea1db33752ec54ac1f8a4a5b776d5b9bacf7
Aaron Evans <aaron.evans@saucelabs.com>
2017-07-24 08:48:09 -0600

    fix indentation on page

--- page.py
+++ page.py
@@ -3,8 +3,8 @@ class SandboxPage:
 	url = "http://saucelabs.github.io/training-test-page/"
 	title = "I am a page title - Sauce Labs"
 
-		def __init__(self, driver):
-			self.driver = driver
-	
-		def open(self):
-			self.driver.get(self.url)
+	def __init__(self, driver):
+		self.driver = driver
+
+	def open(self):
+		self.driver.get(self.url)
