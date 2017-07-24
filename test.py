a3eac689eb4ebfd197c102291e3ed453ca90b3c4
Aaron Evans <aaron.evans@saucelabs.com>
2017-07-24 09:08:22 -0600

    add docstring that checks title

--- test.py
+++ test.py
@@ -21,14 +21,28 @@ driver = webdriver.Remote(
 	desired_capabilities= desired_capabilities
 )
 
-try: 
+def check_title(sandbox):
+	"""
+		It should have the correct title
+
+		>>> check_title(driver)
+		True
+	"""	
+
 	sandbox = SandboxPage(driver)
 	sandbox.open()
-	assert driver.title == SandboxPage.title
+	return sandbox.title == driver.title
 
-finally:
-	driver.quit()
 
-	sauce_client = SauceClient(username, access_key)
-	status = (sys.exc_info() == (None, None, None))
-	sauce_client.jobs.update_job(driver.session_id, passed=status)
+if __name__ == '__main__':
+	try: 
+
+	    import doctest
+	    doctest.testmod()
+	finally:
+		driver.quit()
+
+		sauce_client = SauceClient(username, access_key)
+		status = (sys.exc_info() == (None, None, None))
+		sauce_client.jobs.update_job(driver.session_id, passed=status)
+
